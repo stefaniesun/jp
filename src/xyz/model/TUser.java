0a45769b29a166fdd6c93616d1229a55f308bf81
stefanie <374935367@qq.com>
2016-01-05 17:56:04 +0800

    Signed-off-by: stefanie <374935367@qq.com>

---
+++ src/xyz/model/TUser.java
@@ -0,0 +1,54 @@
+package xyz.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name="TUsers")
+public class TUser {
+
+	
+	@Id
+	@Column(name="UserID",unique=true,nullable=false)
+	@GeneratedValue(generator = "paymentableGenerator")
+    @GenericGenerator(name = "paymentableGenerator", strategy = "identity")
+	private int userID;//主键
+	
+	@Column(name="UserName")
+	private String userName;
+	
+	@Column(name="Pass")
+	private String password;
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+}
